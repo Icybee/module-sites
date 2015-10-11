5371d8189264290df375b036296a24c541181d5b
Olivier Laviale <olivier.laviale@gmail.com>
2015-10-11 18:23:13 +0200

    Added facets

---
+++ config/activerecord.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Icybee\Modules\Sites;
+
+use ICanBoogie\Facets\DateTimeCriterion;
+
+return [
+
+	'facets' => [
+
+		'sites' => [
+
+			'updated_at' => DateTimeCriterion::class
+
+		]
+
+	]
+
+];
